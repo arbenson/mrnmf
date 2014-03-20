ba6cb22ec4e7da5d40e382bfc458566c5575468c
arbenson <arbenson@berkeley.edu>
2014-03-20 11:22:40 -0700

    Fix a small bug

--- GenSyntheticSepLarge.py
+++ GenSyntheticSepLarge.py
@@ -7,7 +7,7 @@ Usage:
 
      dumbo start GenSyntheticSepLarge.py \
      -hadoop $HADOOP_INSTALL \
-	 -m [number of rows in millions] \
+     -m [number of rows in millions] \
      -H [path to r x n coefficient matrix H] \
      -mat [HDFS path to matrix with one million records keys] \
      -epsilon [noise level] \
@@ -39,46 +39,46 @@ gopts = util.GlobalOptions()
 
 class Map:
   def __init__(self, m, mat_name, epsilon):
-	  self.epsilon = epsilon
-	  self.Hprime = []
-	  try:
-		  f = open(mat_name)
-	  except:
-		  f = open(mat_name.split('/')[-1])
-	  for line in f:
-		  row = [float(v) for v in line.split()]
-		  self.Hprime.append(row)
-	  f.close()
+      self.epsilon = epsilon
+      self.Hprime = []
+      try:
+          f = open(mat_name)
+      except:
+          f = open(mat_name.split('/')[-1])
+      for line in f:
+          row = [float(v) for v in line.split()]
+          self.Hprime.append(row)
+      f.close()
 
-	  # matrix dimensions (m in millions)
-	  self.m = m
-	  self.n = len(self.Hprime[0])
-	  self.r = len(self.Hprime)
+      # matrix dimensions (m in millions)
+      self.m = m
+      self.n = len(self.Hprime[0])
+      self.r = len(self.Hprime)
 
-	  self.Hprime = np.array(self.Hprime)
+      self.Hprime = np.array(self.Hprime)
 
   def __call__(self, key, value):
-	  W = np.random.random((self.m, self.r))
-	  M = np.dot(W, np.hstack((np.eye(self.r), self.Hprime)))
-	  # permutation of columns
-	  P = range(self.n)
-	  for i in xrange(self.r):
-		  P[i], P[i * (n / r)] = P[i * (n / r)], P[i]
-	  M = M[:, P]
+      W = np.random.random((self.m, self.r))
+      M = np.dot(W, np.hstack((np.eye(self.r), self.Hprime)))
+      # permutation of columns
+      P = range(self.n)
+      for i in xrange(self.r):
+          P[i], P[i * (self.n / self.r)] = P[i * (self.n / self.r)], P[i]
+      M = M[:, P]
 
-	  # Add noise
-	  N = np.random.random((self.m, self.n)) * self.epsilon
-	  M += N
+      # Add noise
+      N = np.random.random((self.m, self.n)) * self.epsilon
+      M += N
 
-	  for row in M:
-		  key = [np.random.random() for i in xrange(3)]
-		  val = [float(v) for v in row]
-		  yield key, struct.pack('d' * len(val), *val)
+      for row in M:
+          key = [np.random.random() for i in xrange(3)]
+          val = [float(v) for v in row]
+          yield key, struct.pack('d' * len(val), *val)
 
 def runner(job):
-	m = gopts.getintkey('m')
-	epsilon = gopts.getfloatkey('epsilon')
-	H = gopts.getstrkey('H')
+    m = gopts.getintkey('m')
+    epsilon = gopts.getfloatkey('epsilon')
+    H = gopts.getstrkey('H')
     options=[('numreducetasks', '0'), ('nummaptasks', '40')]
     job.additer(mapper=Map(m, H, epsilon), reducer=mrnmf.ID_REDUCER, opts=options)
 
@@ -99,13 +99,13 @@ def starter(prog):
 
     gopts.getstrkey('reduce_schedule', '1')
 
-	gopts.getintkey('m', 200)
-	gopts.getfloatkey('epsilon', 0.0)
+    gopts.getintkey('m', 200)
+    gopts.getfloatkey('epsilon', 0.0)
 
-	H = prog.delopt('H')
-	if not H:
-		return "'H' not specified"
-	gopts.getstrkey('H')
+    H = prog.delopt('H')
+    if not H:
+        return "'H' not specified"
+    gopts.getstrkey('H', 'data/Hprime_20_200.txt')
 
     prog.addopt('file',os.path.join(mypath, 'util.py'))
     prog.addopt('file',os.path.join(mypath, 'mrnmf.py'))
